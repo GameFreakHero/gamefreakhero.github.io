557f8df0b234019babad21d88e28df4a6b459214
GameFreakHero <gamefr3akhero@gmail.com>
2019-07-29 14:59:26 +0200

    added new images and updated bootstrap files

--- js/drawings.js
+++ js/drawings.js
@@ -1,7 +1,7 @@
-for(var i = 36; i > 0; i--){
+for(var i = 38; i > 0; i--){
   var para = document.createElement("img");
   para.src = 'images/drawings/' + i + '.jpg';
-  para.className = "img-responsive, img-circle";
+  para.className = "img-fluid";
 
   var paragr = document.createElement("p");
   paragr.appendChild(para);
